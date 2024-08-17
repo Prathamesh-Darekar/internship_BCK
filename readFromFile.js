a4ed2a9214c6c3fe9ae2bfef1f4c684f45fbf4dd
ChethanTN7 <chethantn947@gmail.com>
2024-08-17 19:40:46 +0530

    project is going on

--- readFromFile.js
+++ readFromFile.js
@@ -1,6 +1,21 @@
 //  THIS IS A MAIN THREAD USED FOR READING DATA FROM FILE - worker thread 1
+let writeToPort = new Worker("writeToPort.js");
+
+if (window.isSecureContext) console.log("secure");
+
+const sharedMemory = new SharedArrayBuffer(1000);
+
+const sharedArray = new Uint8Array(sharedMemory);
+
+writeToPort.postMessage(sharedMemory);
+
+sharedarray[0]  = encoder.encode("000001111100000111110000011111000001111100000111110000011111000001111100000111110000011111000001111100000111110000011111000001111100000111110000011111000001111100000111110000011111000001111100000111110000011111000001111100000111110000011111000001111100000");
+
+
 async function startProcess() {
-  let writeToPort = new Worker("writeToPort.js");
   console.log("Read from file");
+
+  const [fileHandler] = await window.showOpenFilePicker();
+  const file = await fileHandler.getFile();
   writeToPort.postMessage("write to port");
 }
